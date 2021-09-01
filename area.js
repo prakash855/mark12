b65823f0f9011f1f56b4e901022025f8b52b3a69
Prakash Agrahari <sshivamprakashh301@gmail.com>
2021-09-01 16:33:05 +0530

    area page updated with three inputs

--- area.js
+++ area.js
@@ -1,12 +1,34 @@
-const inputs = document.querySelectorAll(".inputs");
-const button = document.querySelector("#btn");
-const output = document.querySelector("#outputArea");
+const firstSide = document.querySelector("#first-side");
+const secondSide = document.querySelector("#second-side");
+const thirdSide = document.querySelector("#third-side");
+const calculate = document.querySelector("#calculate-area");
+const output = document.querySelector("#output");
 
-function calculateArea() {
-  const base = inputs[0].value;
-  const height = inputs[1].value;
-  const area = (1 / 2) * (base * height);
-  output.innerText = `The are of the triangele is: ${area} cm²`;
+function calculateArea(e) {
+  e.preventDefault();
+
+  const firstSideValue = Number(firstSide.value);
+  const secondSideValue = Number(secondSide.value);
+  const thirdSideValue = Number(thirdSide.value);
+
+  if (
+    firstSideValue + secondSideValue > thirdSideValue &&
+    secondSideValue + thirdSideValue > firstSideValue &&
+    firstSideValue + thirdSideValue > secondSideValue
+  ) {
+    const semiPerimeter =
+      (firstSideValue + secondSideValue + thirdSideValue) / 2;
+
+    const result = Math.sqrt(
+      semiPerimeter *
+        (semiPerimeter - firstSideValue) *
+        (semiPerimeter - secondSideValue) *
+        (semiPerimeter - thirdSideValue)
+    ).toFixed(4);
+    output.innerText = `Area of a triangle using heron's formula is ${result} units`;
+  } else {
+    output.innerText = "Enter valid side lengths such that each side lengths";
+  }
 }
 
-button.addEventListener("click", calculateArea);
+calculate.addEventListener("submit", calculateArea);
